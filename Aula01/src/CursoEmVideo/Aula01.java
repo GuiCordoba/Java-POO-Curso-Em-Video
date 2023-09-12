d9e00ad7ab41f8f16bd9485a930524e84df20fcf
Guilherme <guimcordoba@hotmail.com>
2023-09-12 16:08:20 -0300

    Aula01 inserida

---
+++ Aula01/src/CursoEmVideo/Aula01.java
@@ -0,0 +1,25 @@
+package CursoEmVideo;
+
+public class Aula01 {
+
+	public static void main(String[] args) {
+		Caneta c1 = new Caneta();
+		c1.modelo = "Bic";
+		c1.cor = "Azul";
+		c1.ponta = 0.5f;
+		c1.carga = 97;
+		c1.destampar();
+		
+		//c1.status();
+		c1.rabiscar();
+		
+		Caneta c2 = new Caneta();
+		c2.modelo = "Hostnet";
+		c2.cor = "Preta";
+		c2.tampar();
+		c2.rabiscar();
+		
+		
+	}
+
+}
